45fc2d14122b4c28a0b157cce63285b741091c44
a-lone-survivor <990473625@qq.com>
2018-10-24 19:32:38 +0800

    修复错误 & 增加测试用例

--- SensitiveWord.ts
+++ SensitiveWord.ts
@@ -1,9 +1,7 @@
-export default class SensitiveWord
-{
+export default class SensitiveWord {
     private tree: any;
 
-    public setFilterWord(words: string[]): void
-    {
+    public setFilterWord(words: string[]): void {
         // sort
         words.sort((a: string, b: string) => b.length - a.length);
 
@@ -14,19 +12,15 @@ export default class SensitiveWord
         // console.log(JSON.stringify(this.tree, null, '\t'));
     }
 
-    public detectSensitiveWords(content: string): boolean
-    {
+    public detectSensitiveWords(content: string): boolean {
         let node: any = this.tree;
 
-        for (let c of content)
-        {
-            if (c in node)
-            {
+        for (let c of content) {
+            if (c in node) {
                 node = node[c];
 
                 if (node.ended) return true;
-            } else
-            {
+            } else {
                 node = this.tree;
             }
         }
@@ -34,27 +28,51 @@ export default class SensitiveWord
         return false;
     }
 
-    public replaceSensitiveWords(sentence: string, fillChar: string = "*"): string
-    {
+    /**
+     * 替换句子中的敏感字为指定字符
+     * @param sentence
+     * @param fillChar
+     * @param global        外部调用始终为true，false则检查到敏感字后停止后续检查
+     */
+    public replaceSensitiveWords(sentence: string, fillChar: string = "*", global: boolean = true): string {
         let returnValue: string = "";
         let fillChars: string = "";
         let filteredChars: string = "";
 
         let node: any = this.tree;
 
-        for (let c of sentence)
-        {
+        for (let i = 0, len = sentence.length; i < len; i++) {
+            const c = sentence[i];
+
+            // 以当前字符为起点，检查后续是否为敏感字
+            // 有两个前提：
+            // 1. 全局查找时，确保不会被多余的递归调用
+            // 2. 当前节点不是敏感节点树根节点，即正在查找中，如果不在查找中，和后续流程做的事情重复了
+            if (global && node != this.tree) {
+                const sensitiveChars = this.replaceSensitiveWords(sentence.substring(i), fillChar, false);
+                // 如果以当前字符为起始的句子属于敏感句，跳过这段
+                if (sensitiveChars) {
+                    // -1 是为了和 i++ 抵消
+                    i += sensitiveChars.length - 1;
+                    // 需要加上 filteredChars，因为之前的查找只是敏感句的一部分，未构成敏感句
+                    returnValue += filteredChars + sensitiveChars;
+
+                    // 一次敏感词匹配完成完成。重置，准备下次敏感词匹配
+                    fillChars = "";
+                    filteredChars = "";
+
+                    continue;
+                }
+            }
             filteredChars += c;
 
-            if (c in node)
-            {
+            if (c in node) {
                 node = node[c];
 
                 // 如果这个字符在敏感词节点中，则转换成隐藏字符
                 fillChars += fillChar;
 
-                if (node.ended)
-                {
+                if (node.ended) {
                     // 如果这个词构成敏感词，把隐藏字加入结果
                     returnValue += fillChars;
 
@@ -62,9 +80,8 @@ export default class SensitiveWord
                     fillChars = "";
                     filteredChars = "";
                 }
-            } else
-            {
-                node = this.tree;
+            } else {
+                if (!global) return returnValue;
 
                 // 如果这个字符不属于敏感词节点中，允许该字符
                 returnValue += filteredChars;
@@ -72,6 +89,8 @@ export default class SensitiveWord
                 // 不构成完成敏感词，一次敏感词匹配完成完成。重置，准备下次敏感词匹配
                 fillChars = "";
                 filteredChars = "";
+
+                node = this.tree;
             }
         }
 
@@ -83,24 +102,20 @@ export default class SensitiveWord
         return returnValue;
     }
 
-    private addSensitiveWord(word: string): void
-    {
+    private addSensitiveWord(word: string): void {
         let node: any = this.tree;
 
         let char;
         let length: number = word.length;
 
-        for (let i: number = 0; i < length; i++)
-        {
-            char = word[ i ];
+        for (let i: number = 0; i < length; i++) {
+            char = word[i];
 
-            if (char in node)
-            {
-                node = node[ char ];
-            } else
-            {
+            if (char in node) {
+                node = node[char];
+            } else {
                 const newNode: any = {};
-                node[ char ] = newNode;
+                node[char] = newNode;
 
                 node = newNode;
             }
--- test/test.ts
+++ test/test.ts
@@ -1,36 +1,30 @@
 import * as assert from "assert";
-import { readFileSync } from "fs";
-import { join } from "path";
+import {readFileSync} from "fs";
+import {join} from "path";
 import SensitiveWord from "../SensitiveWord";
 
-describe("All", () =>
-{
+describe("All", () => {
     const sensitiveWord: SensitiveWord = new SensitiveWord();
 
-    before(() =>
-    {
-        const sensitiveWords: string[] = readFileSync(join(__dirname, "../../test/sensitive_words"))
+    before(() => {
+        const sensitiveWords: string[] = readFileSync(join(__dirname, "../test/sensitive_words"))
             .toString()
             .split("\n");
         sensitiveWord.setFilterWord(sensitiveWords);
     });
 
-    describe("#detectSensitiveWords", () =>
-    {
-        it("test match", () =>
-        {
+    describe("#detectSensitiveWords", () => {
+        it("test match", () => {
             assert.ok(sensitiveWord.detectSensitiveWords("日了"));
             assert.ok(sensitiveWord.detectSensitiveWords("fuck"));
         });
 
-        it("test not match", () =>
-        {
+        it("test not match", () => {
             assert.ok(!sensitiveWord.detectSensitiveWords("日"));
             assert.ok(!sensitiveWord.detectSensitiveWords("fuc"));
         });
 
-        it("test contains", () =>
-        {
+        it("test contains", () => {
             assert.ok(sensitiveWord.detectSensitiveWords("我日了狗"));
             assert.ok(sensitiveWord.detectSensitiveWords("我日了 狗"));
             assert.ok(sensitiveWord.detectSensitiveWords("我真是日了狗了"));
@@ -40,22 +34,20 @@ describe("All", () =>
         });
     });
 
-    describe("#replaceSensitiveWords", () =>
-    {
-        it("test match", () =>
-        {
+    describe("#replaceSensitiveWords", () => {
+        it("test match", () => {
+            // assert.equal(sensitiveWord.replaceSensitiveWords("微信号+毛泽东"), "*******");
+            assert.equal(sensitiveWord.replaceSensitiveWords("微信号-毛泽东"), "***-***");
             assert.equal(sensitiveWord.replaceSensitiveWords("日了"), "**");
             assert.equal(sensitiveWord.replaceSensitiveWords("fuck"), "****");
         });
 
-        it("test not match", () =>
-        {
+        it("test not match", () => {
             assert.equal(sensitiveWord.replaceSensitiveWords("日"), "日");
             assert.equal(sensitiveWord.replaceSensitiveWords("fuc"), "fuc");
         });
 
-        it("test contains", () =>
-        {
+        it("test contains", () => {
             assert.equal(sensitiveWord.replaceSensitiveWords("我日了狗"), "我**狗");
             assert.equal(sensitiveWord.replaceSensitiveWords("我日了 狗"), "我** 狗");
             assert.equal(sensitiveWord.replaceSensitiveWords("我正是日了狗了"), "我正是**狗了");
@@ -67,8 +59,9 @@ describe("All", () =>
             );
         });
 
-        it("test complex sentence", () =>
-        {
+        it("test complex sentence", () => {
+            assert.equal(sensitiveWord.replaceSensitiveWords("我日fuck"), "我日****");
+            assert.equal(sensitiveWord.replaceSensitiveWords("日了fuck"), "******");
             assert.equal(
                 sensitiveWord.replaceSensitiveWords("我日，真的是日了，fuck you"),
                 "我日，真的是**，**** you",
@@ -76,10 +69,8 @@ describe("All", () =>
         });
     });
 
-    describe("#replaceSensitiveWords with customized character", () =>
-    {
-        it("test contains", () =>
-        {
+    describe("#replaceSensitiveWords with customized character", () => {
+        it("test contains", () => {
             assert.equal(sensitiveWord.replaceSensitiveWords("fuck", "-"), "----");
         });
     });
